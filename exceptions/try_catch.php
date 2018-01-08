d738539249794fe39fe9ec63e3ca35f4ccc8c453
Mamithi <19lawrence93@gmail.com>
2018-01-08 17:29:12 +0300

    Handling errors

---
+++ exceptions/try_catch.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TosPAY
+ * Date: 1/8/2018
+ * Time: 5:23 PM
+ */
+
+try{
+    throw new Exception();
+} catch(Exception $e){
+    echo 'An exception has been caught';
+}
