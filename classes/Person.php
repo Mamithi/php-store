5cf7d7b5d0ee162cdf5eeeb16f05079ad5db458e
Mamithi <19lawrence93@gmail.com>
2018-01-08 16:02:57 +0300

    Constructors for intialization

--- classes/Person.php
+++ classes/Person.php
@@ -3,6 +3,13 @@
 		public $firstname;
 		public $lastname;
 
+		public function __construct(){
+			echo 'Inside person contructor <br/>';
+
+			$this->firstname = '';
+			$this->lastname = '';
+		}
+
 		public function hello($str){
 			return "A person says $str";
 		}
